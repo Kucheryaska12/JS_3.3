3f5cdf824c94b2233195c0d7d91d5b8bbfe217f4
kucheryaska <kucheryaska12@gmail.com>
2026-04-20 21:05:25 +0300

    JS_3.3_v1_hard

--- task.js
+++ task.js
@@ -1,8 +1,16 @@
 const buttonsControl = document.querySelectorAll("a.font-size");
+const colorsControl = document.querySelectorAll("div.book__control_color a.color");
+const colorsBGControl = document.querySelectorAll("div.book__control_background a.color");
 
 
-const func = function(el) {
-    document.querySelector("#book").className = "book";
+const funcFS = function(el) {
+    let reg = /^book_fs-/;
+    for (let i of document.querySelector("#book").classList) {
+        if (reg.test(i)) {
+           document.querySelector("#book").classList.remove(i) 
+        };
+    };
+
     buttonsControl.forEach((button) => {button.classList.remove("font-size_active")});
     el.classList.add("font-size_active");
     if (el.dataset.size != null) {
@@ -10,9 +18,44 @@ const func = function(el) {
     }
 }
 
+const funcColor = function(el) {
+    let reg = /^book_color-/;
+    for (let i of document.querySelector("#book").classList) {
+        if (reg.test(i)) {
+           document.querySelector("#book").classList.remove(i) 
+        };
+    };
+    colorsControl.forEach((color) => {color.classList.remove("color_active")});
+    el.classList.add("color_active");
+    document.querySelector("#book").classList.add(`book_color-${el.dataset.textColor}`)
+}
+
+const funcBG = function(el) {
+    let reg = /^book_bg-/;
+    for (let i of document.querySelector("#book").classList) {
+        if (reg.test(i)) {
+           document.querySelector("#book").classList.remove(i) 
+        };
+    };
+    colorsBGControl.forEach((color) => {color.classList.remove("color_active")});
+    el.classList.add("color_active");
+    document.querySelector("#book").classList.add(`book_bg-${el.dataset.bgColor}`)
+}
 
 buttonsControl.forEach((btn) => {btn.addEventListener("click", (event) => {
     event.preventDefault();
-    func(btn);
+    funcFS(btn);
+    });
+})
+
+colorsControl.forEach((btn) => {btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    funcColor(btn);
+    });
+})
+
+colorsBGControl.forEach((btn) => {btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    funcBG(btn);
     });
 })
